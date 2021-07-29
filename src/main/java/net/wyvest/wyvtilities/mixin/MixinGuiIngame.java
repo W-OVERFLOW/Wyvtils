fd7f0bc66ab2abb542a2c2b3f0845fdba5b59e3b
Wyvest <45589059+Wyvest@users.noreply.github.com>
2021-07-29 23:50:41 +0900

    1.0.0 BETA 1 rewrite action bar and boss bar customization new titles category Title and subtitle scale changer - make titles and subtitles smaller

--- src/main/java/net/wyvest/wyvtilities/mixin/MixinGuiIngame.java
+++ src/main/java/net/wyvest/wyvtilities/mixin/MixinGuiIngame.java
@@ -1,14 +1,19 @@
 package net.wyvest.wyvtilities.mixin;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.gui.GuiIngame;
-import net.wyvest.wyvtilities.bossbar.BossHealth;
+import net.minecraft.client.gui.ScaledResolution;
+import net.minecraft.entity.boss.BossStatus;
+import net.wyvest.wyvtilities.gui.BossHealthGui;
 import net.wyvest.wyvtilities.config.WyvtilsConfig;
-import net.wyvest.wyvtilities.bossbar.BossHealthGui;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.awt.*;
 
 @Mixin(GuiIngame.class)
 public class MixinGuiIngame {
@@ -19,11 +24,46 @@ public class MixinGuiIngame {
             ci.cancel();
             return;
         }
-        if (!WyvtilsConfig.INSTANCE.getBossBarCustomization()) {
-            return;
+        if (WyvtilsConfig.INSTANCE.getBossBarCustomization() && !WyvtilsConfig.INSTANCE.getBossBar()) ci.cancel();
+    }
+
+    @Redirect(method = "renderBossHealth", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/FontRenderer;drawStringWithShadow(Ljava/lang/String;FFI)I"))
+    private int injected(FontRenderer fontRenderer, String text, float x, float y, int color) {
+        if (WyvtilsConfig.INSTANCE.getBossBarCustomization()) {
+            if (WyvtilsConfig.INSTANCE.getBossBarText()) {
+                checkFirstTime();
+                return fontRenderer.drawString(
+                        BossStatus.bossName,
+                        Float.parseFloat(String.valueOf(WyvtilsConfig.INSTANCE.getBossBarX() + 91 - (fontRenderer.getStringWidth(text) / 2))), WyvtilsConfig.INSTANCE.getBossBarY() - 10,
+                        Color.WHITE.getRGB(), WyvtilsConfig.INSTANCE.getBossBarShadow()
+                );
+            } else {
+                return 1;
+            }
+        } else {
+            return fontRenderer.drawStringWithShadow(text, x, y, color);
         }
-        if (WyvtilsConfig.INSTANCE.getBossBar()) BossHealth.INSTANCE.renderBossHealth();
-        ci.cancel();
+    }
+
+    @Redirect(method = "renderBossHealth", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiIngame;drawTexturedModalRect(IIIIII)V"))
+    private void removeBar(GuiIngame guiIngame, int x, int y, int textureX, int textureY, int width, int height) {
+        if (WyvtilsConfig.INSTANCE.getBossBarCustomization()) {
+            if (WyvtilsConfig.INSTANCE.getBossBarBar()) {
+                Minecraft.getMinecraft().ingameGUI.drawTexturedModalRect(WyvtilsConfig.INSTANCE.getBossBarX(), WyvtilsConfig.INSTANCE.getBossBarY(), textureX, textureY, width, height);
+            }
+        } else {
+            Minecraft.getMinecraft().ingameGUI.drawTexturedModalRect(x, y, textureX, textureY, width, height);
+        }
+    }
+
+    private void checkFirstTime() {
+        if (WyvtilsConfig.INSTANCE.getFirstLaunchBossbar()) {
+            WyvtilsConfig.INSTANCE.setFirstLaunchBossbar(false);
+            WyvtilsConfig.INSTANCE.setBossBarX(new ScaledResolution(Minecraft.getMinecraft()).getScaledWidth() / 2 - 91);
+            WyvtilsConfig.INSTANCE.setBossBarY(12);
+            WyvtilsConfig.INSTANCE.markDirty();
+            WyvtilsConfig.INSTANCE.writeData();
+        }
     }
 
 }
