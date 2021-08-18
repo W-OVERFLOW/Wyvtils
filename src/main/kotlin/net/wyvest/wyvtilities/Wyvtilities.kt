8b292dfe5e5fd346e42b099081ec092ad1f9ed5a
Wyvest <45589059+Wyvest@users.noreply.github.com>
2021-08-18 22:44:52 +0900

    1.2.0 Beta 2 initial release of 1.17.1 version!! :D

--- src/main/kotlin/net/wyvest/wyvtilities/Wyvtilities.kt
+++ src/main/kotlin/net/wyvest/wyvtilities/Wyvtilities.kt
@@ -1,24 +1,16 @@
 package net.wyvest.wyvtilities
 
 import net.fabricmc.api.ClientModInitializer
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents
 import net.fabricmc.loader.api.FabricLoader
-import net.minecraft.client.util.InputUtil
 import net.wyvest.wyvtilities.config.WyvtilsConfig
-import org.lwjgl.glfw.GLFW
 import java.io.File
 
+@Suppress("unused")
 object Wyvtilities : ClientModInitializer {
     override fun onInitializeClient() {
         if (!File(File(FabricLoader.getInstance().configDir.toFile(), "Wyvtilities"), "1.17.1").exists()) {
             File(File(FabricLoader.getInstance().configDir.toFile(), "Wyvtilities"), "1.17.1").mkdirs()
         }
         WyvtilsConfig.initialize()
-        ClientTickEvents.END_CLIENT_TICK.register {
-            if (it.currentScreen != null) return@register
-            if (InputUtil.isKeyPressed(it.window.handle, GLFW.GLFW_KEY_Z)) {
-                it.setScreen(WyvtilsConfig.gui())
-            }
-        }
     }
 }
