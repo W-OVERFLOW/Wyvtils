ffde6c5e956760dbed4d98599506690bc974d324
Wyvest <45589059+Wyvest@users.noreply.github.com>
2021-10-18 14:09:27 +0900

    Finish base implementation for 1.17.1

---
+++ 1.17.1/src/main/kotlin/xyz/qalcyo/wyvtils/seventeen/Wyvtils.kt
@@ -0,0 +1,58 @@
+/*
+ * Wyvtils, a utility mod for 1.8.9.
+ * Copyright (C) 2021 Wyvtils
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package xyz.qalcyo.wyvtils.seventeen
+
+import gg.essential.api.EssentialAPI
+import net.fabricmc.api.ClientModInitializer
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper
+import net.fabricmc.loader.api.FabricLoader
+import net.minecraft.client.option.KeyBinding
+import net.minecraft.client.util.InputUtil
+import org.lwjgl.glfw.GLFW
+import xyz.qalcyo.wyvtils.core.MinecraftVersions
+import xyz.qalcyo.wyvtils.core.WyvtilsCore
+import xyz.qalcyo.wyvtils.core.config.WyvtilsConfig
+import java.io.File
+
+object Wyvtils: ClientModInitializer {
+    private val keyBinding: KeyBinding = KeyBindingHelper.registerKeyBinding(
+        KeyBinding(
+            "key.wyvtils.keybind", // The translation key of the keybinding's name
+            InputUtil.Type.KEYSYM, // The type of the keybinding, KEYSYM for keyboard, MOUSE for mouse.
+            GLFW.GLFW_KEY_Z, // The keycode of the key
+            "category.wyvtils.ok" // The translation key of the keybinding's category.
+        )
+    )
+
+    override fun onInitializeClient() {
+        WyvtilsCore.modDir = File(File(File(FabricLoader.getInstance().configDir.toFile(), "Qalcyo"), "Wyvtils"), "1.17.1")
+        if (!WyvtilsCore.modDir!!.exists()) {
+            WyvtilsCore.modDir!!.mkdirs()
+        }
+        WyvtilsCore.jarFile = File(javaClass.protectionDomain.codeSource.location.toURI())
+        WyvtilsCore.onInitialization(MinecraftVersions.SEVENTEEN)
+        ClientTickEvents.END_CLIENT_TICK.register {
+            while (keyBinding.wasPressed() && it.currentScreen == null) {
+                EssentialAPI.getGuiUtil().openScreen(WyvtilsConfig.gui())
+                return@register
+            }
+        }
+    }
+}
