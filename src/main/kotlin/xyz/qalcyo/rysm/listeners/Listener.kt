c2cc6485c571f6689f17ba9895ea2bb2c23a5cc9
Wyvest <45589059+Wyvest@users.noreply.github.com>
2021-10-25 12:07:35 +0900

    Update Listener.kt

--- src/main/kotlin/xyz/qalcyo/rysm/listeners/Listener.kt
+++ src/main/kotlin/xyz/qalcyo/rysm/listeners/Listener.kt
@@ -203,6 +203,7 @@ object Listener {
                     else -> ChatColor.WHITE.toString()
                 }
                 changeTextColor = false
+                HighlightManager.cache.invalidateAll()
             }
         }
     }
